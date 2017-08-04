4ea9e806a11fd55e877447698c2cc24d6335fbb5
Denis <dennissber@gmail.com>
2017-08-04 08:11:35 +0300

    new

--- html/function.php
+++ html/function.php
@@ -1,1 +1,71 @@
-<? ?>
+<?php
+header('Content-Type: text/html; charset=utf-8');
+ini_set('display_errors',true);
+error_reporting(E_ALL);
+require_once 'data/menu.php';
+require_once 'data/categories.php';
+require_once 'data/products.php';
+/*Построение дерева категорий*/
+function makeTree($categories,$parent_id=0) {
+    $results=array();
+    if ($categories) {
+        foreach ($categories as $key => $category) {
+            if ($category->parent_id==$parent_id) {
+                if ($category->id!=$parent_id) {
+                    $subcategories = makeTree($categories,$category->id);
+                    if(!empty($subcategories))
+                        $category->subcategories = $subcategories ;
+                }
+                $results[]=$category;
+                unset($category);
+            }
+        }
+    }
+    return $results;
+}
+/*Вывод дерева категорий*/
+function viewCategories($categories) {
+    if($categories) { // проверка лишней не бывает
+        echo "<ul>";
+        foreach ($categories as $category) {
+            if($category->visible) { //важная проверка, которая позволит выводит только включенные категории на сайте
+                echo
+                "<li>
+                    <a href='?r=categories&id=".$category->id."'>$category->name</a>
+                </li>";
+                if(isset($category->subcategories)) {
+                    // проверяем есть ли подкатегории и вызываем заново функцию для вывода
+                    viewCategories($category->subcategories); // передаем в функцию уже массив обьектов покатегорий
+                }
+            }
+        }
+        echo "</ul>";
+    }
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+function viewMenu($pages) {
+    if($pages) {
+        echo '<ul class="nav navbar-nav">';
+        foreach ($pages as $page) {
+            if ($page->visible && $page->menu_id == 1) {
+                if($page->url == '') {
+                    echo "<li class='nav-link'><a href='".$_SERVER['SCRIPT_NAME']."'>$page->name</a></li>";
+                } else {
+                    echo "<li class='nav-link'><a href='?r=pages&id=".$page->id."'>$page->name</a></li>";
+                }
+            }
+        }
+        echo '</ul>';
+    }
+}
+function getPage($pages,$page_id) {
+    if($page_id) {
+        return $pages[$page_id];
+    }
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+function getProduct($id) {
+    if($id) {
+        //дальше сами)
+    }
+}
